5107d74707822e792db8692aad01fa214d02ce72
Samuel <samuel.guerrero6@educa.madrid.org>
2023-04-28 12:15:04 +0200

    Intento instanciar el objeto creado en la otra rama pero no se puede ya que en esta rama no existe esa clase

--- EjerciciGitEclipse/src/Main.java
+++ EjerciciGitEclipse/src/Main.java
@@ -5,6 +5,10 @@ public class Main {
 		System.out.println("Listado de alumnos 1º DAW");
 		
 		System.out.println("Alumno1");
+		
+		Alumno pepe = new Alumno("pepe", "Jimenez", "3423234B");
+		
+		
 	}
 
 }
