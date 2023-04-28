050fa20f263e151ec696701a66135e205c1ca3b5
Samuel <samuel.guerrero6@educa.madrid.org>
2023-04-28 12:09:26 +0200

    Añado constructor con los 3 parametros

--- EjerciciGitEclipse/src/Alumno.java
+++ EjerciciGitEclipse/src/Alumno.java
@@ -3,4 +3,13 @@ public class Alumno {
 	
 	private String nombre, apellido, dni;
 
+	private Alumno(String nombre, String apellido, String dni) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	}
+	
+	
+
 }
